027e8e4449dd8e9b41d30401b0f844f02dbf6876
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-01 17:02:11 -0500

    completed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,62 @@
+import java.sql.SQLOutput;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("How are you?");
+
+        int myFavoriteNumber;
+        myFavoriteNumber = 5;
+        System.out.println(myFavoriteNumber);
+
+        String myString;
+        myString = "Aaron";
+        System.out.println(myString);
+        myString = "f";
+        System.out.println(myString);
+//        myString = 3.14159;
+//        System.out.println(myString);
+        long myNumber;
+        myNumber = 123;
+        System.out.println(myNumber);
+//        float myNewNumber;
+//        myNewNumber = 3.14;
+//        System.out.println(myNewNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        System.out.println(three);
+
+//        int x = 4;
+//        x = x + 5;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        x /= y;
+//        y -= x;
+//        System.out.println(y);
+
+//        byte num;
+//        num = 10000;
     }
 }
