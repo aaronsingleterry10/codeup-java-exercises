b84f763c4adb7569c2a09d045cef7b5c4d67a334
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-02 12:19:05 -0500

    completed a step

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f", pi);
+
+//        System.out.println("Enter a number:");
+//        int userInput = sc.nextInt();
+//        System.out.println(userInput);
+
+        System.out.println("Enter three different words:");
+        String userInput1 = sc.next();
+        String userInput2 = sc.next();
+        String userInput3 = sc.next();
+        System.out.printf("%s%n%s%n%s",userInput1, userInput2, userInput3);
+
+    }
 }
