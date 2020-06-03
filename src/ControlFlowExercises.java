993059c523efdd0b51894829cafaa3d8f770d976
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-03 10:53:41 -0500

    completed step

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -33,8 +33,19 @@ public class ControlFlowExercises {
 //        for (int i = 100; i >= -10; i -= 5) {
 //            System.out.println(i);
 //        }
-        for (long i = 2; i < 1000000; i *= i) {
-            System.out.println(i);
+//        for (long i = 2; i < 1000000; i *= i) {
+//            System.out.println(i);
+//        }
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
