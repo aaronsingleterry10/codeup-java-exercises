860be1c2aaa984ecd75da05e65d36d849d79e89e
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-03 12:04:25 -0500

    completed exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,4 @@
-import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
@@ -36,16 +36,79 @@ public class ControlFlowExercises {
 //        for (long i = 2; i < 1000000; i *= i) {
 //            System.out.println(i);
 //        }
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.println("Enter a number:");
+//        int userInput = sc.nextInt();
+//        System.out.println("Here is your table:");
+//        System.out.println("number | squared | cubed");
+//        System.out.println("------ | ------- | -----");
+//        for (int i = 1; i <= userInput; i++) {
+//            if (i * i < 10) {
+//                System.out.println(i + "      | " + (i * i) + "       | " + (i * i * i));
+//            } else {
+//                System.out.println(i + "      | " + (i * i) + "      | " + (i * i * i));
+//            }
+//        }
+//        sc.nextLine();
+        System.out.println("Would you like to continue? yes/no");
+        String response;
+        int grade;
+        while (true) {
+            response = sc.nextLine();
+            if (response.equals("no")) {
+                System.out.println("Have a good day!");
+                break;
+            } else if (response.equals("yes")) {
+                System.out.println("Let's continue!");
+                System.out.println("Enter in a number between 0-100 to receive a grade.");
+                grade = sc.nextInt();
+                if (grade >= 98) {
+                    System.out.println("Your grade is an A+!");
+                } else if (grade <= 97 && grade >= 95) {
+                    System.out.println("Your grade is an A!");
+                } else if (grade <= 94 && grade >= 90) {
+                    System.out.println("Your grade is an A-");
+                } else if (grade <= 89 && grade >= 88) {
+                    System.out.println("Your grade is a B+");
+                } else if (grade <= 87 && grade >= 85) {
+                    System.out.println("Your grade is a B");
+                } else if (grade <= 84 && grade >= 80) {
+                    System.out.println("Your grade is a B-");
+                } else if (grade <= 79 && grade >= 78) {
+                    System.out.println("Your grade is a C+");
+                } else if (grade <= 77 && grade >= 75) {
+                    System.out.println("Your grade is a C");
+                } else if (grade <= 74 && grade >= 70) {
+                    System.out.println("Your grade is a C-");
+                } else {
+                    System.out.println("You've received an F!!");
+                }
+                System.out.println("Would you like to try again?");
+                sc.nextLine();
             } else {
-                System.out.println(i);
+                System.out.println("Invalid response");
+                System.out.println("Would you like to continue? yes/no");
             }
         }
+
+//        if (response.equals("no")) {
+//            System.out.println("Have a good day!");
+//        } else if (response.equals("yes")) {
+//            System.out.println("Let's continue!");
+//        } else {
+//            System.out.println("Invalid response");
+//        }
     }
 }
