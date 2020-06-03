eda24cee570a91e3089cf7f57cf3a36a4acc6440
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-03 10:36:27 -0500

    completed problem

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -19,10 +19,22 @@ public class ControlFlowExercises {
 //            i -= 5;
 //        } while (i >= -10);
 
-        long i = 2;
-        do {
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i = i * i;
+//        } while (i < 1000000);
+//        for (int i = 5; i <= 15; i++) {
+//            System.out.print(i + " ");
+//        }
+//        for (int i = 0; i <= 100; i += 2) {
+//            System.out.println(i);
+//        }
+//        for (int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+        for (long i = 2; i < 1000000; i *= i) {
             System.out.println(i);
-            i = i * i;
-        } while (i < 1000000);
+        }
     }
 }
