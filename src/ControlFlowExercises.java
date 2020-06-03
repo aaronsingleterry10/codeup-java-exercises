d17dfda6eaa8ca922d24c0224827399910e9e2bd
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-03 09:38:20 -0500

    completed a couple of problems

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,20 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.print(i + " ");
-            ++i;
-        }
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.print(i + " ");
+//            ++i;
+//        }
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i <= 100);
+//    }
+        int i = 100;
+        do {
+            System.out.println(i);
+            i -= 5;
+        } while (i >= -10);
     }
 }
