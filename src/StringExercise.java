4e71b143074e652189f2580cb8f0ff1bccaf7db6
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-04 14:56:33 -0500

    completed first part of exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,13 @@
 public class StringExercise {
     public static void main(String[] args) {
 
+        String text = "random text";
+//        System.out.println(text.replace("random text", "We don't need no education\nWe don't need no thought control"));
+//        System.out.println(text.replace("random text", "Check \"this\" out!, \"s inside of \"s!"));
+//        System.out.println(text.replace("random text", "In windows, the main drive is usually C:\\"));
+//        System.out.println(text.replace("random text", "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslashes \\\\\\"));
+
+
+
     }
 }
