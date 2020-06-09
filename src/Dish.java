5372bf2443e901f5d61e7b61428c56032f28b426
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-09 11:59:59 -0500

    completed first exercise

--- src/Dish.java
+++ src/Dish.java
@@ -1,2 +1,10 @@
 public class Dish {
+
+    public int costInCents;
+    public String nameOfDish;
+    public boolean wouldRecommend;
+
+    public void printSummary() {
+        System.out.printf("Cost: %d\nName: %s\nRecommended: %s", costInCents, nameOfDish, wouldRecommend);
+    }
 }
--- src/DishTest.java
+++ src/DishTest.java
@@ -1,2 +1,9 @@
 public class DishTest {
+    public static void main(String[] args) {
+        Dish d1 = new Dish();
+        d1.costInCents = 9;
+        d1.nameOfDish = "Double Whataburger with cheese";
+        d1.wouldRecommend = true;
+        d1.printSummary();
+    }
 }
