2fe1e6f6198cb3af06dd986018da1a8c7c065594
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-10 11:28:10 -0500

    completed step

--- src/Person.java
+++ src/Person.java
@@ -16,10 +16,25 @@ public class Person {
 
     public static void main(String[] args) {
         Person p1 = new Person("Kali");
-        System.out.println(p1.name);
+//        System.out.println(p1.name);
         Person p2 = new Person("Tex");
         Person p3 = new Person("Mabel");
-        p2.sayHello();
-        p3.sayHello();
+//        p2.sayHello();
+//        p3.sayHello();
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
     }
 }
