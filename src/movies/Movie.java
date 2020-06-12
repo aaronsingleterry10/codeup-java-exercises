43745e76ca1b78da122ebe48a9f45ddec0cbad04
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-12 14:56:03 -0500

    created movies array

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -3,5 +3,24 @@ package movies;
 public class Movie {
     private String name;
     private String category;
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
 }
