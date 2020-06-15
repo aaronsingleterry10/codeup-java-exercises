4c608d493daab0a224752758e58cdabe3176ad59
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-14 20:13:13 -0500

    completed bonus

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,6 +1,7 @@
 package movies;
 import util.Input;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -18,6 +19,17 @@ public class MoviesApplication {
     public static String getAllMovies() {
         String output = "";
         for (Movie movie: MoviesArray.findAll()) {
+            output += movie.getName() + " - " + movie.getCategory() + "\n";
+        }
+        return output;
+    }
+
+    public static String addNewMovie(Movie[] array, String movieName, String movieGenre) {
+        String output = "";
+        Movie m = new Movie(movieName, movieGenre);
+        Movie[] newMovieArray = Arrays.copyOf(array, array.length + 1);
+        newMovieArray[newMovieArray.length - 1] = m;
+        for (Movie movie: newMovieArray) {
             output += movie.getName() + " - " + movie.getCategory() + "\n";
         }
         return output;
@@ -41,7 +53,7 @@ public class MoviesApplication {
 //            }
 //        }
         System.out.println("Welcome to our Movies Application! What would you like to do?");
-        System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category");
+        System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category\n" + "6 - add new movie");
         String genreInput = userInput.getString();
         do {
             if (genreInput.equals("0")) {
@@ -49,24 +61,32 @@ public class MoviesApplication {
                 break;
             } else if (genreInput.equals("1")) {
                 System.out.println(getAllMovies());
-                System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category");
+                System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category" + "6 - add new movie");
                 genreInput = userInput.getString();
             } else if (genreInput.equals("2")) {
                 System.out.println(getMovieName("animated"));
-                System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category");
+                System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category" + "6 - add new movie");
                 genreInput = userInput.getString();
             } else if (genreInput.equals("3")) {
                 System.out.println(getMovieName("drama"));
-                System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category");
+                System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category" + "6 - add new movie");
                 genreInput = userInput.getString();
             } else if (genreInput.equals("4")) {
                 System.out.println(getMovieName("horror"));
-                System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category");
+                System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category" + "6 - add new movie");
                 genreInput = userInput.getString();
             } else if (genreInput.equals("5")) {
                 System.out.println(getMovieName("scifi"));
-                System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category");
+                System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category" + "6 - add new movie");
                 genreInput = userInput.getString();
+            } else if (genreInput.equals("6")) {
+                System.out.println("Enter name");
+                String name = userInput.getString();
+                System.out.println("Enter genre");
+                String genre = userInput.getString();
+                System.out.println(addNewMovie(MoviesArray.findAll(), name, genre));
+                scanner.next();
+
             } else {
                 System.out.println("Invalid response");
                 System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n" + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n" + "5 - view movies in the scifi category");
