d866e7f93b062005c4845e052b737710222dd1df
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-16 15:36:42 -0500

    created measurable interface

--- src/Blender.java
+++ src/Blender.java
@@ -7,4 +7,10 @@ public class Blender extends Appliances {
     public void doWork() {
         System.out.println("Blending a smoothie...");
     }
+
+    public static void main(String[] args) {
+        Appliances b1 = new Blender();
+        b1.doWork();
+        b1.makeSound();
+    }
 }
--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,19 +1,19 @@
 package shapes;
 
 public class Rectangle {
-    protected int width;
-    protected int length;
-
-    public Rectangle(int width, int length) {
-        this.width = width;
-        this.length = length;
-    }
-
-    public int getArea() {
-        return this.width * this.length;
-    }
-
-    public int getPerimeter() {
-        return (this.width * 2) + (this.length * 2);
-    }
+//    protected int width;
+//    protected int length;
+//
+//    public Rectangle(int width, int length) {
+//        this.width = width;
+//        this.length = length;
+//    }
+//
+//    public int getArea() {
+//        return this.width * this.length;
+//    }
+//
+//    public int getPerimeter() {
+//        return (this.width * 2) + (this.length * 2);
+//    }
 }
