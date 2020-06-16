1178fec15bd515b1bf25ef36ae06c332949d172a
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-16 13:47:17 -0500

    added methods and properties to appliances class and also created refridgerator class

--- src/Appliances.java
+++ src/Appliances.java
@@ -1,2 +1,32 @@
 abstract class Appliances {
+    String brand;
+    String color;
+
+    public Appliances() {
+    }
+
+    public Appliances(String brand, String color) {
+        this.brand = brand;
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    abstract void makeSound();
+
+    abstract void doWork();
 }
