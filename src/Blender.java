1ef49043e3b23656904547a03ce51bda35a1c798
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-16 13:53:18 -0500

    added methods to refridgerator class and created blender class

---
+++ src/Blender.java
@@ -0,0 +1,10 @@
+public class Blender extends Appliances {
+
+    public void makeSound() {
+        System.out.println("Making blender sounds...");
+    }
+
+    public void doWork() {
+        System.out.println("Blending a smoothie...");
+    }
+}
--- src/Refridgerator.java
+++ src/Refridgerator.java
@@ -1,2 +1,10 @@
 public class Refridgerator extends Appliances {
+    public void makeSound() {
+        System.out.println("Making whatever sound fridges make...");
+    }
+
+    public void doWork() {
+        System.out.println("Making stuff cold...");
+    }
+
 }
