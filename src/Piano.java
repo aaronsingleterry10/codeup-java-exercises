283a1bea1d82a5eb85d6d7296b3ac3bde28dd6d3
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-17 11:14:26 -0500

    added methods/constructor in piano class

--- src/Piano.java
+++ src/Piano.java
@@ -1,2 +1,32 @@
-public class Piano {
+public class Piano extends MusicalInstrument implements Tunable, VolumeAdjustable {
+
+    public Piano(String inst) {
+        super(inst);
+    }
+
+    public void startMusic() {
+        System.out.println("Playing Bettie and the Jets...");
+    }
+
+    public void stopMusic() {
+        System.out.println("Fingers off keys now...");
+    }
+
+    public void tuneInstrument() {
+        System.out.println("Tuning piano...");
+    }
+
+    public void detuneInstrument() {
+        System.out.println("Detune piano...");
+    }
+
+    public void volumeUp() {
+        System.out.println("Opening up the piano lid thing more...");
+    }
+
+    public void volumeDown() {
+        System.out.println("Closing the piano lid thing...");
+    }
+
+
 }
