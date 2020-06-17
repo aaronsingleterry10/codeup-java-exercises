91441624be101312019f79b9701829b211d6525c
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-17 10:11:17 -0500

    added methods

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,30 @@
 package shapes;
 
-public class Square extends Rectangle {
-//    public Square(int side) {
+public class Square extends Quadrilateral {
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return this.length * 4;
+    }
+
+    @Override
+    public double getArea() {
+        return this.length * this.length;
+    }
+
+    //    public Square(int side) {
 //        super(side, side);
 //    }
 //
