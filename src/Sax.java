ed49df6265e99903100c646f312c979a6ea4f3b7
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-17 11:27:26 -0500

    added methods/constructor to sax class

--- src/Sax.java
+++ src/Sax.java
@@ -1,2 +1,36 @@
-public class Sax {
+public class Sax extends MusicalInstrument implements Tunable, VolumeAdjustable {
+
+    public Sax(String inst) {
+        super(inst);
+    }
+
+    @Override
+    public void startMusic() {
+
+    }
+
+    @Override
+    public void stopMusic() {
+
+    }
+
+    @Override
+    public void tuneInstrument() {
+
+    }
+
+    @Override
+    public void detuneInstrument() {
+
+    }
+
+    @Override
+    public void volumeUp() {
+
+    }
+
+    @Override
+    public void volumeDown() {
+
+    }
 }
