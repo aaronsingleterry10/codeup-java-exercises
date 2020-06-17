da1f5f260f4b643a168224820a555e68b3b13aa4
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-17 10:10:42 -0500

    added code

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,4 +1,23 @@
 package shapes;
 
 abstract class Quadrilateral extends Shape implements Measurable {
+    protected double length;
+    protected double width;
+
+    public Quadrilateral(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    abstract void setLength(double length);
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    abstract void setWidth(double width);
 }
