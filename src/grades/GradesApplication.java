54a1be35f4d8e64e05da5ac71ee9305ac5a27070
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-18 15:07:11 -0500

    completed main exercise

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,10 +1,13 @@
 package grades;
 
+import util.Input;
+
 import java.util.HashMap;
 
 public class GradesApplication {
     public static void main(String[] args) {
         HashMap<String, Student> students = new HashMap<>();
+        Input in = new Input();
         students.put("codesoldier", new Student("John"));
         students.put("hackerattacker", new Student("Steve"));
         students.put("syntaxwarrior", new Student("Cody"));
@@ -25,6 +28,26 @@ public class GradesApplication {
         System.out.println("Welcome!");
         System.out.println("Here are the GitHub usernames of our students:");
         System.out.println("|codesoldier|  |hackerattacker|  |syntaxwarrior|");
-        System.out.println("What student would you like to see more information on?");
+        String input;
+        do {
+            System.out.println("What student would you like to see more information on?");
+            input = in.getString();
+            if (students.containsKey(input)) {
+                System.out.println("Name: " + students.get(input).getName() + " - GitHub Username: " + input);
+                System.out.println("Current average: " + students.get(input).getGradeAverage());
+                System.out.println("Would you like to see another student? y/n");
+                input = in.getString();
+                if (input.equalsIgnoreCase("n")) {
+                    System.out.println("Goodbye!");
+                }
+            } else {
+                System.out.println("Sorry, no student found with the GitHub username of " + "\"" + input + "\".");
+                System.out.println("Would you like to see another student? y/n");
+                input = in.getString();
+                if (input.equalsIgnoreCase("n")) {
+                    System.out.println("Goodbye!");
+                }
+            }
+        } while (input.equalsIgnoreCase("y"));
     }
 }
