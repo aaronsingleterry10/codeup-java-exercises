de3d48bc6b8bbddf60cb56042d574b8adc4b9238
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-20 13:43:40 -0500

    completed bonus

--- src/ExtraArrayExercises.java
+++ src/ExtraArrayExercises.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ExtraArrayExercises {
-//    1. Create a command line grocery list maker
+    //    1. Create a command line grocery list maker
 //​
 //    - before prompting the user to enter each item, ask how many items will be
 //            entered
@@ -12,14 +12,43 @@ public class ExtraArrayExercises {
 //    objects to store in an array
 //    - once the user has entered all items, print out all items grouped by
 //    category and alphabetized in each group​
+    public static void groceryList() {
+//        GroceryItem[] groceryItems;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Welcome to Grocery List Maker.\nHow many grocery items will you be entering?");
+//        int numItemsEntered = sc.nextInt();
+//        groceryItems = new GroceryItem[numItemsEntered];
+//        for (int i = 0; i < groceryItems.length; i++) {
+//
+//            System.out.println("Please enter the name of grocery item #" + (i + 1) + ":");
+//            String nameOfGroceryItem = sc.next();
+//
+//            System.out.println("Please enter the quantity:");
+//            int quantityOfItem = sc.nextInt();
+//
+//            System.out.println("Please enter the category of item");
+//            String categoryOfItem = sc.next();
+//            groceryItems[i] = new GroceryItem(categoryOfItem, nameOfGroceryItem, quantityOfItem);
+//        }
+//        try {
+//            GroceryItem[] newGroceryItem = Arrays.sort(groceryItems);
+//        } catch (Exception e) {
+//            System.out.println("something went wrong");
+//        }
+//        for (GroceryItem groceryItem : groceryItems) {
+//            System.out.println(groceryItem.getCategory() + " " + groceryItem.getName() + " " + groceryItem.getQuantity());
+//        }
+    }
 
     public static void main(String[] args) {
-        GroceryItem[] groceryItems;
-        Scanner sc = new Scanner(System.in);
 
+        GroceryItem[] groceryItems;
+        String[] newGroceryList;
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Grocery List Maker.\nHow many grocery items will you be entering?");
         int numItemsEntered = sc.nextInt();
         groceryItems = new GroceryItem[numItemsEntered];
+        newGroceryList = new String[numItemsEntered];
         for (int i = 0; i < groceryItems.length; i++) {
 
             System.out.println("Please enter the name of grocery item #" + (i + 1) + ":");
@@ -31,9 +60,25 @@ public class ExtraArrayExercises {
             System.out.println("Please enter the category of item");
             String categoryOfItem = sc.next();
             groceryItems[i] = new GroceryItem(categoryOfItem, nameOfGroceryItem, quantityOfItem);
+
         }
-        for (GroceryItem groceryItem : groceryItems) {
-            System.out.println(groceryItem.getCategory() + " " + groceryItem.getName() + " " + groceryItem.getQuantity());
+
+        for (int i = 0; i < groceryItems.length; i++) {
+           newGroceryList[i] = "Category: " + groceryItems[i].getCategory() + ". Name of item: " + groceryItems[i].getName() + ". Quantity: " + groceryItems[i].getQuantity() + ". ";
         }
+        System.out.println(Arrays.toString(newGroceryList));
+
+        Arrays.sort(newGroceryList);
+        System.out.println(Arrays.toString(newGroceryList));
+        String list = "";
+        for (int i = 0; i < newGroceryList.length; i++) {
+            if (i != newGroceryList.length - 1) {
+                list += newGroceryList[i] + "\n==================\n";
+            } else {
+                list += newGroceryList[i];
+            }
+        }
+
+        System.out.println(list);
     }
 }
