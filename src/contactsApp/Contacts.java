5914b65f0d5f70636060a3996abd4e09a8a4de63
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-22 19:41:45 -0500

    refactored some code

--- src/contactsApp/Contacts.java
+++ src/contactsApp/Contacts.java
@@ -1,27 +1,37 @@
 package contactsApp;
 
 public class Contacts {
-    private String name;
-    private String number;
+    private String firstName;
+    private String lastName;
+    private long number;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
+    public Contacts(String firstName, String lastName, long number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.number = number;
     }
 
-    public Contacts(String name, String number) {
-        this.name = name;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
         this.number = number;
     }
 }
