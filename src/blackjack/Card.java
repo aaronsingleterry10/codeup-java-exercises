baa3dece0df2b0f519c190383535f1d795dfba58
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-26 23:43:38 -0500

    pushing up to git

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -3,9 +3,11 @@ package blackjack;
 public class Card {
     private String face;
     private String suit;
+    private int value;
 
     public int getValue() {
 //- Write a method named `getValue` that returns the blackjack value of the
 //      card
+        return this.value;
     }
 }
