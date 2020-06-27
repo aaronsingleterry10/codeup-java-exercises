35ac4e81b4631fb56e887ce4224974edd5aed28e
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-26 23:21:45 -0500

    added properties

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -1,4 +1,11 @@
 package blackjack;
 
 public class Card {
+    private String face;
+    private String suit;
+
+    public int getValue() {
+//- Write a method named `getValue` that returns the blackjack value of the
+//      card
+    }
 }
