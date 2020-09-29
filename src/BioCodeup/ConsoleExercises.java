9ece5edb5dd387ac95e609ec6f5b7526fd9820a1
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-09-29 13:57:36 -0500

    pushing up

--- src/BioCodeup/ConsoleExercises.java
+++ src/BioCodeup/ConsoleExercises.java
@@ -1,4 +1,20 @@
 package BioCodeup;
 
+import java.util.Scanner;
+
 public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f", pi);
+//        System.out.println("Enter an integer");
+//        int userNum = sc.nextInt();
+//        System.out.printf("You have entered the integer: %d", userNum);
+//        System.out.println("Enter in 3 words");
+//        String firstWord = sc.nextLine();
+//        String secondWord = sc.nextLine();
+//        String thirdWord = sc.nextLine();
+//        System.out.printf("You have entered \"%s\", \"%s\", and \"%s\"", firstWord, secondWord, thirdWord);
+
+    }
 }
