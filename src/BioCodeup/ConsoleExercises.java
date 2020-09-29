6bcbfc6dede201a28e3afa901c446f6854c12752
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-09-29 14:19:08 -0500

    completed lec

--- src/BioCodeup/ConsoleExercises.java
+++ src/BioCodeup/ConsoleExercises.java
@@ -1,5 +1,6 @@
 package BioCodeup;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ConsoleExercises {
@@ -15,6 +16,15 @@ public class ConsoleExercises {
 //        String secondWord = sc.nextLine();
 //        String thirdWord = sc.nextLine();
 //        System.out.printf("You have entered \"%s\", \"%s\", and \"%s\"", firstWord, secondWord, thirdWord);
+        System.out.println("Enter in the width of the room");
+        int width = Integer.parseInt(sc.nextLine());
 
+        System.out.println("Enter in the length of the room");
+        int length = Integer.parseInt(sc.nextLine());
+
+        System.out.println("The area of your room is: " + (width * length));
+        System.out.println("The perimeter of your room is: " + ((width * 2) + (length * 2)));
+        System.out.printf("The area of your room is %d\n", (width * length));
+        System.out.printf("The perimeter of your room is: %d\n", ((width * 2) + (length * 2)));
     }
 }
