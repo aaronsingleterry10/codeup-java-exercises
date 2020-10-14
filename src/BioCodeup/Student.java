e87aae1996b08617ab2ca27eb9b6cec5bdbaf6d0
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-10-14 15:28:51 -0500

    added properties, constructor, some getters/setters

--- src/BioCodeup/Student.java
+++ src/BioCodeup/Student.java
@@ -1,4 +1,36 @@
 package BioCodeup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
+    private String name;
+    private List<Integer> grades;
+
+    public Student(String name, ArrayList<Integer> grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        double sumOfGrades = 0;
+        double numberOfGrades = 0;
+        for (int x : this.grades) {
+            numberOfGrades += 1;
+            sumOfGrades += x;
+        }
+        return sumOfGrades / numberOfGrades;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
